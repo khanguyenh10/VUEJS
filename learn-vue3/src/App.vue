850e23d60856df3f7e464d2a65ac2309c5cba10e
khanguyenh10 <khanguyenh10dev@gmail.com>
2025-11-03 17:26:41 +0700

    update

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
+<script setup>
+  import  SyntaxCompoent   from "./components/Syntax.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+   <SyntaxCompoent />
 </template>
 
 <style scoped></style>
