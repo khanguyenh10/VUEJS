0d4fa9ed544667a9fb6151f461d227553407aed1
khanguyenh10 <khanguyenh10dev@gmail.com>
2025-11-04 17:13:29 +0700

    classBinding

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-  import  SyntaxComponent   from "./components/Syntax.vue";
-  import  ReactivelyComponent   from "./components/Reactively.vue";
-  import  ComputedComponent   from "./components/Computed.vue";
+import SyntaxComponent from './components/Syntax.vue'
+import ReactivelyComponent from './components/Reactively.vue'
+import ComputedComponent from './components/Computed.vue'
+import ClassStyleBindingComponent from './components/ClassStyleBinding.vue'
 </script>
 
 <template>
-   <!-- <SyntaxComponent /> -->
-    <!-- <ReactivelyComponent /> -->
-     <ComputedComponent />
+  <!-- <SyntaxComponent /> -->
+  <!-- <ReactivelyComponent /> -->
+  <!-- <ComputedComponent /> -->
+  <ClassStyleBindingComponent />
 </template>
 
 <style scoped></style>
---
+++ learn-vue3/src/components/ClassStyleBinding.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref, reactive, computed} from 'vue'
+const isActive = ref(true);
+const hasError = ref(false);
+// const classObject = reactive({
+//     active: true,
+//     'has-error': true
+// })
+
+const classObject = computed(() => ({
+    active: isActive.value && !hasError.value,
+    'has-error': hasError.value
+}));
+
+const changeActive = () => {
+    isActive.value = !isActive.value;
+}
+</script>
+
+<template>
+    <h1>ClassStyleBinding</h1>
+    <!-- 
+      ClassBinding
+      - viết class trong style và gán vào class của thẻ HTML
+      - gán class động với object 
+      - gán class động với array
+
+    -->
+    <div>
+        <!-- <button class="btn" :class="{active: isActive, 'has-error': hasError}">Active </button> -->
+        <button @click="changeActive" class="btn" :class="classObject">{{isActive ? 'Active': 'Inactive'}} </button>
+        <button @click="changeActive" class="btn" :class="[isActive ? 'active': '', {'has-error': hasError}, 'has-warning']">{{isActive ? 'Active': 'Inactive'}} </button>
+    </div>
+</template>
+
+<style>
+.btn{
+     border:none;
+    background: #ede9fe;
+    color: #fff;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;    
+    &:hover{
+        background: #c7d2fe;
+    }
+}
+.active{
+    background: #8b5cf6;
+    &:hover{
+        background: #7c3aed;
+    }
+}
+.has-error{
+     background: #ef4444;
+    &:hover{
+        background: #e92929ff;
+    }
+}
+</style>
