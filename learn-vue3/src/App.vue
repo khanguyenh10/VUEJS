7be766aec660822deadfc256bf7ee614b0ec56d9
khanguyenh10dev <khanguyenh10dev@gmail.com>
2025-11-14 12:02:06 +0700

    update eventHandling

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -5,8 +5,9 @@ import ComputedComponent from './components/03-Computed.vue'
 // import ClassStyleBindingComponent from './components/ClassStyleBinding.vue'
 // import StyleBindingComponent from './components/05-StyleBinding.vue'
 // import ConditionComponent from './components/06-Condition.vue'
-import List from './components/07-List.vue'
-import TodoList from './pratice/08-TodoList.vue'
+// import List from './components/07-List.vue'
+// import TodoList from './pratice/TodoList.vue'
+import EventHandling from './components/08-EventHandling.vue'
 </script>
 
 <template>
@@ -17,7 +18,8 @@ import TodoList from './pratice/08-TodoList.vue'
   <!-- <StyleBindingComponent /> -->
   <!-- <ConditionComponent /> -->
    <!-- <List/> -->
-   <TodoList/>
+   <!-- <TodoList/> -->
+    <EventHandling/>
 </template>
 
 <style scoped></style>
---
+++ learn-vue3/src/components/08-EventHandling.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, reactive, computed, nextTick} from 'vue'
+const count = ref(0);
+const sayHi = (name)=>{
+    alert(`Xin chào ${name}`);
+}
+const submit = ()=>{
+    alert('Submit');
+}
+</script>
+
+<template>
+    <h1>Event Handling</h1>
+    <!--
+       click event
+       v-on:click="clickHandler"
+       @click="clickHandler"
+       @click.stop="clickHandler" : ngan chan bubbling
+       @clic.self="clickHandler" : chỉ click trên chính element đó
+    -->
+     <div>
+        <button v-on:click="count++">Thêm 1</button>    
+        <button @click="sayHi('A')">Chào A</button>
+        <button @click="sayHi('B')">
+            Chào B
+            <button @click.stop="sayHi('C')">Chào C</button>
+        </button>
+         <div @click.self="sayHi('D')">
+                 <button>Chào D</button>
+            </div>
+        <p>Count: {{ count }}</p>
+        <form @submit.prevent.stop="submit">
+            <!-- nếu ko chỉ type mặc định là submit -->
+            <button type="submit">Submit</button> 
+        </form>
+        <br/>
+        <input placeholder="Nhập tên của bạn" @keyup.enter.alt="submit"> <!-- nhấn alt + enter--><br/><br/>
+        <button @click.shift="submit">người dùng nhấn chuột đồng thời giữ phím Shift.</button>
+    </div>
+</template>
+
