c71e0a99c040747490d7ae9cf80366bbc8a2b40c
khanguyenh10 <khanguyenh10dev@gmail.com>
2025-11-17 14:29:52 +0700

    update

--- learn-vue3/src/lessons/06-Condition.vue
+++ learn-vue3/src/lessons/06-Condition.vue
@@ -9,26 +9,33 @@ const  toggleLoggedIn =  () => {
 const  toggleVisible =  () => {
    isVisible.value = !isVisible.value;
 }
-const useStatus = ref('online');
+const userStatus = ref('online');
 const changeUserStatus = () =>{
-    if(useStatus.value === 'online'){
-        useStatus.value = 'away';
-    }else if(useStatus.value === 'away'){
-        useStatus.value = 'busy';
-    }else if (useStatus.value === 'busy'){
-        useStatus.value = 'online';
+    if(userStatus.value === 'online'){
+        userStatus.value = 'away';
+    }else if(userStatus.value === 'away'){
+        userStatus.value = 'busy';
+    }else if (userStatus.value === 'busy'){
+        userStatus.value = 'online';
     }
 }
 </script>
 
 <template>
+    <!--
+        Conditional Rendering
+        - v-if: hiển thị hoặc ẩn thẻ HTML dựa trên điều kiện
+        - v-else-if: thêm điều kiện khác nếu v-if sai
+        - v-else: nếu tất cả điều kiện trên đều sai thì hiển thị thẻ HTML này
+        - v-show: ẩn hoặc hiển thị thẻ HTML bằng cách thay đổi thuộc tính display:none
+    -->
     <h1 v-if="isLoggined">Chào mừng người dùng đã đăng nhập</h1>
     <h1 v-else>Chào mừng khách</h1>
     <button @click="toggleLoggedIn">{{isLoggined ? 'Đăng xuất' : 'Đăng nhập'}}</button>
     <br/>
-    <h1 v-if="useStatus == 'online'" >Người dùng đang online</h1>
-    <h1 v-else-if="useStatus == 'away'">Người dùng đang vắng mặt</h1>
-    <h1 v-else-if="useStatus == 'busy'">Người dùng đang bận</h1>
+    <h1 v-if="userStatus == 'online'" >Người dùng đang online</h1>
+    <h1 v-else-if="userStatus == 'away'">Người dùng đang vắng mặt</h1>
+    <h1 v-else-if="userStatus == 'busy'">Người dùng đang bận</h1>
     <h1 v-else>Không rõ trạng thái</h1>
     <button @click="changeUserStatus">thay đổi trạng thái</button>
     <br/>
--- learn-vue3/src/lessons/07-List.vue
+++ learn-vue3/src/lessons/07-List.vue
@@ -1,41 +1,45 @@
 <script setup>
-import { ref, reactive, computed, nextTick} from 'vue'
+import { ref, reactive, computed, nextTick } from 'vue'
 
-const shoppingItems = ref([{
+const shoppingItems = ref([
+  {
     name: 'apple',
     price: 7,
-    isActive: true
-},{
+    isActive: true,
+  },
+  {
     name: 'orange',
     price: 10,
-    isActive: false 
-}   
-]);
+    isActive: false,
+  },
+])
 // v-for cho objectS
 const user = ref({
-    fistname: 'John',
-    lastname: 'Doe',
-    age: 30
+  fistname: 'John',
+  lastname: 'Doe',
+  age: 30,
 })
 </script>
 
 <template>
-     <div>
-           <!-- V-for cho danh sách -->
-            <!-- :key giúp Vue cập nhật đúng phần tử, tránh lỗi và giúp chạy nhanh hơn. -->
-        <ul>
-            <li v-for="item in shoppingItems" :key="item.name">{{ item.name }}: {{ item.price }}</li>
-        </ul>
-         <!-- V-for cho một dãy số -->
-        <span v-for="n in 10">{{ n }}</span>
-         <!-- V-for cho object -->
-        <p v-for="(value, key, index) in user" :key="key">{{ key }}: {{ value }}</p>
-        <!-- V-for kết hợp với v-if -->
-        <ul>
-            <template  v-for="item in shoppingItems" :key="item.name">
-            <li v-if="item.isActive">{{ item.name }}: {{ item.price }}</li>
-            </template>
-        </ul>
-     </div>
+  <div>
+    <!-- 
+            Rendering theo danh sách
+              - dùng v-for để lặp qua một mảng và hiển thị các phần tử
+              - :key giúp Vue cập nhật đúng phần tử, tránh lỗi và giúp chạy nhanh hơn.
+     -->
+    <ul>
+      <li v-for="item in shoppingItems" :key="item.name">{{ item.name }}: {{ item.price }}</li>
+    </ul>
+    <!-- V-for cho một dãy số -->
+    <span v-for="n in 10">{{ n }}</span>
+    <!-- V-for cho object -->
+    <p v-for="(value, key, index) in user" :key="key">{{ key }}: {{ value }}</p>
+    <!-- V-for kết hợp với v-if -->
+    <ul>
+      <template v-for="item in shoppingItems" :key="item.name">
+        <li v-if="item.isActive">{{ item.name }}: {{ item.price }}</li>
+      </template>
+    </ul>
+  </div>
 </template>
-
